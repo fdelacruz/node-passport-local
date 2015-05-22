62ac8a0b5f2e399df8efb6ccfa37a32b5c10b7f0
Francisco De La Cruz <helloquico@gmail.com>
2015-05-22 00:03:22 -0400

    Compare Passwords after Encryption

--- models/user.js
+++ models/user.js
@@ -44,5 +44,20 @@ userSchema.pre('save', function(next) {
 	});
 });
 
+userSchema.methods.comparePassword = function(
+		candidatePassword, next) {
+	// compare the saved, encrypted password
+	// to the user-entered one
+	bcrypt.compare(
+		candidatePassword, this.password, 
+		function(err, isMatch) {
+			if (err) {
+				return next(err);
+			}
+			next(null, isMatch);
+		}
+	);
+};
+
 var User = mongoose.model('User', userSchema);
 module.exports = User;
