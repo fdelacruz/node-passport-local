ae2775b7c8bb26ec9567bb28fddf0a355755f56d
Francisco De La Cruz <helloquico@gmail.com>
2015-05-22 17:42:59 -0400

    User Logout

--- routes/authentication.js
+++ routes/authentication.js
@@ -29,4 +29,9 @@ router.post('/login', function(req, res, next) {
 	})(req, res, next);
 });
 
+router.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/auth/login');
+});
+
 module.exports = router;
